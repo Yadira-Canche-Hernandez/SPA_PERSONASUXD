667c9a847572ffbfe471a3943de4ff70bda35c0a
Yadira-Canche-Hernandez <yadira.19070010@itsmotul.edu.mx>
2023-03-08 20:49:27 -0600

    Componente BotonEnviar.vue

---
+++ src/components/BotonEnviar.vue
@@ -0,0 +1,14 @@
+<script>
+
+export default {
+    props: ['desactivar'],
+}
+</script>
+
+<template>
+    <div class="flex justify-center items-center space-x-4 text-base my-8">
+        <button class="h-14 mx-2 sm:h-10 px-7 font-semibold rounded-md bg-slate-800 text-white my-2 justify-center shadow-xl hover:shadow-inner transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105" type="button">
+            <slot></slot>
+        </button>
+    </div>
+</template>
