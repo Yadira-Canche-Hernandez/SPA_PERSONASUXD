cfe2cf9d3c6ea42a2e3b5d75c421053ab4fbc021
Yadira-Canche-Hernandez <yadira.19070010@itsmotul.edu.mx>
2023-03-08 23:11:12 -0600

    Componente TextArea

---
+++ src/components/TextArea.vue
@@ -0,0 +1,25 @@
+<script>
+
+export default {
+    
+    data() {
+        return {
+            datoTA: '',
+        }
+    },
+    methods: {
+        //Funcion para hacer un emit
+        PasarDato() {
+            
+            this.$emit('dataTA',this.datoTA)
+        }
+    }
+}
+</script>
+
+<template>
+    
+    <textarea type="text" v-model="datoTA" class="w-full py-2.5 px-4 rounded-lg bg-gray-50 focus:shadow focus:bg-white focus:outline-none" id="TextArea" rows="2" @input="PasarDato">
+    </textarea>
+
+</template>
