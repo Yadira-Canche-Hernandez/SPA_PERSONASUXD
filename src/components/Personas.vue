c98a57089ee1e4eaf8a51e4ccd3127f79eb1fe40
Yadira-Canche-Hernandez <yadira.19070010@itsmotul.edu.mx>
2023-03-09 17:24:52 -0600

    BUSCADOR listo, por id

--- src/components/Personas.vue
+++ src/components/Personas.vue
@@ -10,6 +10,11 @@ export default {
         //variables de retorno  
         //para lista de personas
         personas: [],
+        mostrarLista: false,
+        mostrarBuscados: false,
+        nohay:false,
+        buscar:"",
+        buscados:[],
       }
     },
     mounted() {
@@ -21,69 +26,98 @@ export default {
           this.personas = response.data.personas
           console.log (this.personas)
       })
+      
     },
     methods: {
       //funcion para buscar personajes
       buscador(buscar)  { 
         if(buscar==""){
-          this.mostrarListas= true
+          this.mostrarLista= true
           this.mostrarBuscados = false
-          this.mostrarBuscadoID = false
+          
+          
         }
         else{
-          this.mostrarListas= false
-          console.log(!isNaN (buscar)) //comprobando que funcione con la funcion isNan
-          //si lo que recibe del input es un numero
-          if(!isNaN (buscar) === true) {
-            //busca al personaje por su id
-            API_URL='https://rickandmortyapi.com/api/character/'+buscar
-            axios.get(API_URL) //usando axios
-            .then((response) => {
-              //toma todos los personajes que encuentra en resultados y lo almacena en la variable buscados
-              console.log(response.data)
-              this.buscado = response.data;
-              console.log(this.buscado)
-            })
-            //si es por ir para que muestre
-            this.mostrarBuscadoID = true
-            //mantenemos en falso para que no muestre datos de la tarjeta de todos los personajes
-            this.mostrarBuscados = false
-          }
+          this.mostrarLista= false
           
-          //si no es numerico
-          else{
+
+          if(!isNaN (buscar) === true) {
+          API_URL='/api/getPersonasUxd.php?id='+ buscar
+
+          axios.get(API_URL) 
+          .catch(error => {
+             
+             this.mostrarBuscados = false
+             this.nohay = true
+           })
+          
+          .then((response) => {
             
-            //lo busca por nombre                
-            API_URL='https://rickandmortyapi.com/api/character/?name='+buscar
-            axios.get(API_URL) //usando axios
-            .then((response) => {
-              //toma todos los personajes que encuentra en resultados y lo almacena en la variable buscados
-              this.buscados = response.data.results;
-              console.log(this.buscados)
-              
-            })
-            //devolvemos falso por id
-            this.mostrarBuscadoID = false
-            //cambiamos a verdadero que recibe por nombre 
+            this.buscados = response.data.persona;
+            console.log(this.buscados)
             this.mostrarBuscados = true
+            this.nohay = false
             
-            }
-              console.log(API_URL) //comprobando API_URL en consola
-          }
-      },
+          })
+
+          console.log(this.nohay)
+          
+          //cambiamos a verdadero que recibe por nombre 
+          this.nohay=true           
+          } 
+               
+        }
+        
+      }
     }
 }
 </script>
 
 <template>
-<!--Recorre la nueva lista y por cada personaje que encuentra-->    
-<div class="w-2/3 my-5 mx-auto p-3 border-4 rounded-lg bg-black mb-5">
-    <h1 class="text-center text-white text-4xl font-bold mt-4 "> Lista de Personas</h1><br>
+
+  <!--Buscador-->
+  <div class="flex justify-center items-center space-x-4 text-base my-2">
+    <input class="h-12 wl-5 px-7" type="text" v-model="buscar"  placeholder="Buscar por nombre o id" @input = "buscador(buscar)">
+
+  </div>
     
-    <div class="text-2xl my-5 text-black mx-auto" >
+  <!--Resultados Buscador-->
+
+
+  <!--si no recibe true en la variable mostrarBuscados
+      ENCONTRO VARIOS PERSONAJES POR SU NOMBRE-->
+  <div v-if="mostrarBuscados">
+   
+    <!--Recorre la nueva lista y por cada personaje que encuentra-->    
+    <div class=" my-5 mx-auto p-3 border-4 border-black bg-white rounded-sm text-black">
+     
+      <!--carta de personaje-->          
+      <h2> <b>Id: </b> {{ buscados.id }}</h2>
+      <h2> <b>Nombre:</b> {{ buscados.nombre }}</h2>
+      <h4> <b>Edad:</b> {{ buscados.edad }}</h4>
+      <h4> <b>Estado Civil:</b> {{ buscados.estadoCivil }}</h4>
+     
+    </div> 
+    
+  </div>
+
+  <div v-if="nohay" class="mx-auto">
+
+    <h1 class="text-center text-xl my-5">
+      No existe el usuario que buscas :0
+    </h1>
+
+  </div>
+
+
+  <!--Recorre la nueva lista y por cada personaje que encuentra-->    
+  <div class="w-2/3 my-5 mx-auto p-3 border-4 rounded-lg bg-black mb-5 mt-6">
+
+    <h1 class="text-center text-white text-4xl font-bold mt-4"> Lista de Personas</h1><br>
+    
+    <div class="text-2xl text-black mx-auto">
         
-        
-        <table class = "table stripped bordered border-black border-4 text-base mx-auto">
+        <table class = "table border-black border-4 text-base mx-auto">
           <thead >
             <tr class="border-black border-4 bg-emerald-500">
               <th class="border-black border-4">Id</th>
@@ -94,17 +128,19 @@ export default {
             </tr>
           </thead>
           <tbody>
-            <tr v-for="p in personas" class=" border-black border-4">
-              <td class=" border-black border-2 mx-3  bg-white py-3 px-3 ">{{ p.id }}</td>
-              <td class=" border-black border-2 mx-3  bg-white py-3 px-3">{{ p.nombre }}</td>
-              <td class=" border-black border-2 mx-3  bg-white py-3 px-3">{{ p.edad }}</td>
-              <td class=" border-black border-2 mx-3  bg-white py-3 px-3">{{ p.trabajo }}</td>
-              <td class=" border-black border-2 mx-3  bg-white py-3 px-3">{{ p.residencia }}</td>
+            <tr v-for="(p, index) in personas"  class=" border-black border-4">
+              <td class=" border-black border-2 mx-3  bg-white py-2 px-3 ">  {{ index += 1 }} </td>
+              <td class=" border-black border-2 mx-3  bg-white py-2 px-3">{{ p.nombre }}</td>
+              <td class=" border-black border-2 mx-3  bg-white py-2 px-3">{{ p.edad }}</td>
+              <td class=" border-black border-2 mx-3  bg-white py-2 px-3">{{ p.trabajo }}</td>
+              <td class=" border-black border-2 mx-3  bg-white py-2 px-3">{{ p.residencia }}</td>
+              
             </tr>
+            
           </tbody>
         </table>
 
-      </div>
-      
     </div>
+        
+  </div>
 </template>
