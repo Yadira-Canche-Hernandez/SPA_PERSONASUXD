4a3948381d8fefd50418ecfe122ddbd498c28120
Yadira-Canche-Hernandez <yadira.19070010@itsmotul.edu.mx>
2023-03-09 13:14:41 -0600

    Ya muestra la lista.- estaba mal mi response

--- src/components/Personas.vue
+++ src/components/Personas.vue
@@ -1,30 +1,25 @@
 <script>
 import axios from 'axios'
 
-
-let API_URL = 'http://isclab.com.mx/personasuxd/getPersonasUxd.php'
+let API_URL = '/api/getPersonasUxd.php'
 
 export default {
 
   data() {
     return {
         //variables de retorno  
-        //para lista de personajes
-        info: [],
+        //para lista de personas
         personas: [],
-        mostrarListas: true,
-        cont:2,
       }
     },
     mounted() {
-    
-    
-      axios.get(API_URL)
-      .then((response) => {
-        //paginas
-        this.personas = response.personas;
-        console.log (this.personas)
-        
+      axios
+        .get(API_URL)
+          .then((response) => {
+          
+          //lista
+          this.personas = response.data.personas
+          console.log (this.personas)
       })
   },
 }
@@ -35,7 +30,7 @@ export default {
 <div class=" my-5 mx-auto p-3 border-4 border-black bg-white rounded-sm">
       
     <div class="text-2xl my-10 text-black mx-5 sm:py-2" >
-        <h1 > Personas por página</h1><br>
+        <h1 > Lista de Personas</h1><br>
         <ul class="text-lg">
           <li v-for="p in personas">
             <!--Llamando función obtener info personal por personaje-->
