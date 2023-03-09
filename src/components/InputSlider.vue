ee42117cebd1ffd36f169254044e429a0562afbe
Yadira-Canche-Hernandez <yadira.19070010@itsmotul.edu.mx>
2023-03-09 00:14:46 -0600

    Componente Slider

---
+++ src/components/InputSlider.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+    
+    data() {
+        return {
+            porcentaje: 50
+        }
+    },
+    methods: {
+        PasarDato() {
+            this.$emit('porcentaje',this.porcentaje)
+        }
+    }
+}
+</script>
+
+<template>
+    <input type="range" v-model="porcentaje" class="w-full py-3 px-5 rounded-lg transition-colors bg-gray-100 focus:shadow focus:bg-white focus:outline-none" id="porcentaje" placeholder="" typeof="slider" min="0" max="100" @input="PasarDato"/>
+</template>
